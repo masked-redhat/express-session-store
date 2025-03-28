9c10f2078127b50fbef1a37e0aa3a3d242c7b8c1
Zed <skhull.coding@proton.me>
2025-03-28 11:00:19 +0530

    feat: check for valid json object

--- utils/checks.js
+++ utils/checks.js
@@ -1,1 +1,9 @@
 export const isNumber = (value) => typeof value === "number" && value !== NaN;
+
+export const isValidJsonObject = (value) => {
+  try {
+    JSON.stringify(value);
+    return true;
+  } catch {}
+  return false;
+};
